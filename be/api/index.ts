0de534df95ab6332a2762d302591768648431d70
peerhadi <haditawus@proton.me>
2026-06-15 18:22:33 +0530

    Task: made backend for vercel

--- be/api/index.ts
+++ be/api/index.ts
@@ -1,6 +1,9 @@
-import { app } from "../src/server.js";
+import { buildApp } from "../src/app.js";
+
+const app = await buildApp();
 
 export default async function handler(req: any, res: any) {
   await app.ready();
+
   app.server.emit("request", req, res);
 }
